c5f68f7412aba05f517581248301785e99685a9f
Administrator <Administrator@41148E0FF02E50D>
2023-06-20 14:24:05 +0530

    Add project files.

---
+++ EmployeeManagementAppSQL/Employee.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementAppSQL
+{
+    internal class Employee
+    {
+        SqlConnection con = new SqlConnection("server=41148E0FF02E50D;database=EmployeeManagement;integrated security=true");
+
+        public string AddNewEmployee(int p_empNo, string p_empname, string p_empDesignation, double p_empSalary, bool p_isPerm)
+        {
+            SqlCommand cmd_insert = new SqlCommand("insert into EmployeeDetails values(@empNo, @empName, @empDesignation, @empSalary, @empIsPerm)", con);
+            cmd_insert.Parameters.AddWithValue("empNo", p_empNo);
+            cmd_insert.Parameters.AddWithValue("empName", p_empname);
+            cmd_insert.Parameters.AddWithValue("empDesignation", p_empDesignation);
+            cmd_insert.Parameters.AddWithValue("empSalary", p_empSalary);
+            cmd_insert.Parameters.AddWithValue("empIsPerm", p_isPerm);
+
+            try
+            {
+                con.Open();
+                int result = cmd_insert.ExecuteNonQuery(); //methods returns number of rows affected in database
+                if(result == 1)
+                {
+                    con.Close();
+                    return "Employee Added to database";
+                }
+                else
+                {
+                    con.Close();
+                    return "Could not insert Please check with admin";
+                }
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+                return ex.Message;
+            }
+        }
+
+        public string DeleteEmployee(int p_empNo)
+        {
+            SqlCommand cmd_delete = new SqlCommand("delete from EmployeeDetails where empNo=@empNo", con);
+            cmd_delete.Parameters.AddWithValue("empNo", p_empNo);
+
+            try
+            {
+                con.Open();
+                int result = cmd_delete.ExecuteNonQuery();
+                if(result == 1)
+                {
+                    con.Close();
+                    return "Employee Deleted successfully";
+                }
+                else
+                {
+                    con.Close();
+                    return "Could not delete Employee, please check again";
+                }
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+                return ex.Message;
+            }
+        }
+
+        public string UpdateEmployee(int p_empNo, string p_empname, string p_empDesignation, double p_empSalary)
+        {
+            SqlCommand cmd_update = new SqlCommand("update EmployeeDetails set empName=@empName, empDesignation=@empDesignation, empSalary=@empSalary where empNo=@empNo", con);
+            cmd_update.Parameters.AddWithValue("empName", p_empname);
+            cmd_update.Parameters.AddWithValue("empDesignation", p_empDesignation);
+            cmd_update.Parameters.AddWithValue("empSalary", p_empSalary);
+            cmd_update.Parameters.AddWithValue("empNo", p_empNo);
+
+            try
+            {
+                con.Open();
+                int result = cmd_update.ExecuteNonQuery();
+                if(result == 1)
+                {
+                    con.Close();
+                    return "Employee details updated successfully";
+                }
+                else
+                {
+                    con.Close();
+                    return "Could not update detail Please check again";
+                }
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ EmployeeManagementAppSQL/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeManagementAppSQL;
+
+Console.WriteLine("Welcome to Employee Management");
+
+Console.WriteLine("Press Enter to continue");
+Console.ReadLine();
+
+Console.WriteLine("1. Add New Employee");
+Console.WriteLine("2. Delete Employee");
+Console.WriteLine("3. Update Employee");
+Console.WriteLine("4. View Employee Detail");
+Console.WriteLine("5. View All Employee");
+
+int choice  = Convert.ToInt32(Console.ReadLine());
+Employee empObj = new Employee();
+
+switch (choice)
+{
+    case 1:
+        Console.WriteLine("Enter Employee Number");
+        int no = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Employee Name");
+        string name  = Console.ReadLine();
+
+        Console.WriteLine("Enter Employee Designation");
+        string desg = Console.ReadLine();
+
+        Console.WriteLine("Enter Employee Salary");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter id employee is permanent");
+        bool perm = Convert.ToBoolean(Console.ReadLine());
+
+        string result = empObj.AddNewEmployee(no, name, desg, salary, perm);
+        Console.WriteLine(result);
+        break;
+    case 2:
+        Console.WriteLine("Please enter employee number");
+        int eNo = Convert.ToInt32(Console.ReadLine());
+
+        string resp=empObj.DeleteEmployee(eNo);
+        Console.WriteLine(resp);
+        break;
+
+    case 3:
+        Console.WriteLine("Please enter empployee number");
+        int empNo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("please Enter the name to be updated");
+        string empName = Console.ReadLine();
+
+        Console.WriteLine("Please enter new designation");
+        string empDesg = Console.ReadLine();
+
+        Console.WriteLine("please enter updated salary");
+        double empSal = Convert.ToDouble(Console.ReadLine());
+
+        string response = empObj.UpdateEmployee(empNo, empName, empDesg, empSal);
+        Console.WriteLine(response);
+        break;
+
+    default:
+        break;
+}
